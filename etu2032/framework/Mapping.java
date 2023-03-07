19d87a8bcce21e1a2600aafc8f4d6b760731983f
ManoarySarobidy <103274612+ManoarySarobidy@users.noreply.github.com>
2023-03-07 11:24:44 +0300

    First Commit
    Update of etu2032.framework.servlet.FrontServlet.java
    Add of etu002032.framework.Mapping.java

---
+++ etu2032/framework/Mapping.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2032.framework;
+
+/**
+ *
+ * @author sarobidy
+ */
+public class Mapping {
+    String className;
+    String method;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    
+}
