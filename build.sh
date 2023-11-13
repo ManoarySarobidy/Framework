992f34755e4c2d336b1ea8580953c21d12e1bbc6
ManoarySarobidy <103274612+ManoarySarobidy@users.noreply.github.com>
2023-11-13 05:16:44 +0300

    Update build.sh
    removed args

--- build.sh
+++ build.sh
@@ -8,16 +8,16 @@ FRAMEWORK="./etu2032"
 TOMCAT=$HOME/Desktop/tomcat/webapps
 
 cd './etu2032/'
-./compile.sh $1 
+./compile.sh framework
 cd '..'
 # mkdir "$TEST"/lib
 LIB="$TEST"/lib
-cp "$FRAMEWORK/$1.jar" "$LIB"
+cp "$FRAMEWORK/framework.jar" "$LIB"
 cp "$FRAMEWORK/gson-2.10.1.jar" "$LIB"
 
 # Script 2
 cd "$TEST"
-./predeploy.sh $2
+./predeploy.sh testFramework
 
-cp "$2.war" $TOMCAT
+cp "$2.war" ../
 
